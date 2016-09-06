13b45afea4f65e3138f1e28bd3adee3bb437d08f
Nate Gray <ngray311@gmail.com>
2016-09-06 19:25:23 -0400

    Initial Commit

---
+++ CSC376 - Assignment 1 - Problem 3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSC376 - Assignment 1 - Problem 3
+//
+//  Created by Gray, Nathan on 9/6/16.
+//  Copyright © 2016 Gray, Nathan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
